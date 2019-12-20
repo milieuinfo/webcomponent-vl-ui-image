b2832933419f53144656f9173c1d0bd5f13a8f2e
Pascal Riquier <riquipa@riquipa.local>
2019-12-20 14:09:12 +0100

    First commit

---
+++ vl-image.src.js
@@ -0,0 +1,19 @@
+import { VlElement, define } from '/node_modules/vl-ui-core/vl-core.js';
+
+/**
+ * VlImage
+ * @class
+ * @classdesc 
+ * 
+ * @extends VlElement
+ * 
+ * @property 
+ * 
+ * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-image/releases/latest|Release notes}
+ * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-image/issues|Issues}
+ * @see {@link https://webcomponenten.omgeving.vlaanderen.be/demo/vl-image.html|Demo}
+ * 
+ */
+export class VlImage extends VlElement(HTMLElement) {}
+
+define('vl-image', VlImage);
