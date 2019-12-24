77470ab9e3dce1b27fcc285ee5327629587b9a9f
Tom Coemans <tom.coemans@gmail.com>
2019-12-24 10:47:34 +0100

    overbodige code verwijderd

--- vl-image.src.js
+++ vl-image.src.js
@@ -7,29 +7,14 @@ import { NativeVlElement, define } from '/node_modules/vl-ui-core/vl-core.js';
  * 
  * @extends NativeVlElement
  * 
- * @property 
- * 
  * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-image/releases/latest|Release notes}
  * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-image/issues|Issues}
  * @see {@link https://webcomponenten.omgeving.vlaanderen.be/demo/vl-image.html|Demo}
  * 
  */
 export class VlImage extends NativeVlElement(HTMLImageElement) {
-
-   static get _observedAttributes() {
-        return [];
-    }
-
-    static get _observedClassAttributes() {
-        return [];
-    }
-
     connectedCallback() {
         this.classList.add('vl-image');
-    }
-
-    get _classPrefix() {
-        return 'vl-image--';
     }
 
     get _stylePath() {
